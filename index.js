612cf43cea4b58cb1d4e67a9ae66f0f597cecde4
Marcelo Romero <romemarce1@gmail.com>
2024-09-09 12:38:52 -0300

    Directorio publico

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const cors = require("cors");
 // Crear servidor express
@@ -6,12 +7,19 @@ const app = express();
 
 app.use(cors());
 // Directorio publico
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
+// Middleware para servir archivos estáticos desde "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Lectura y parseo del body
 app.use(express.json())
 
 // Rutas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Carga el archivo HTML principal
+});
+
 app.use('/api', require('./src/routes/news.routes'))
 
 
