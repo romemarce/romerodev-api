3b4a773885a89d9adb2ceb5a86693e602cd09582
Marcelo Romero <romemarce1@gmail.com>
2024-09-09 12:41:53 -0300

    Optimize

--- src/services/new.services.js
+++ src/services/new.services.js
@@ -2,7 +2,7 @@ async function serviceGetNews(category = "general") {
   let articles = [];
   const posts = require(`../data/news/${category}.json`);
 
-  articles.push(posts);
+  articles.push(posts.articles);
 
   const data = { articles };
   return data;
