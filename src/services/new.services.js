04b29acbe98be3511f3ac1f52686586a93ad25dd
Marcelo Romero <romemarce1@gmail.com>
2024-09-09 12:55:28 -0300

    Optimiza respuesta

--- src/services/new.services.js
+++ src/services/new.services.js
@@ -1,11 +1,6 @@
 async function serviceGetNews(category = "general") {
-  let articles = [];
   const posts = require(`../data/news/${category}.json`);
-
-  articles.push(posts.articles);
-
-  const data = articles;
-  return data;
+  return posts.articles;
 }
 
 module.exports = serviceGetNews;
